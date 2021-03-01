901d9fa6b6c68e2c3626b3b233c2448d3c7271d7
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2021-03-01 16:07:35 -0300

    Estrutura do projeto

---
+++ src/test/java/br/com/rodrigoamora/fornecedor/FornecedorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora.fornecedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
